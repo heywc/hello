6c24d8274cf9464195539d0af7b29b95af6e5d96
1842347744 <1842347744@qq.com>
2022-09-07 22:34:32 +0800

    添加了xxx功能

---
+++ src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class=''>
+
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {},
+    data () {
+        return {
+        
+        }
+    },
+    created() {
+    
+    },
+    mounted() {
+    
+    },
+    methods: { 
+    
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
